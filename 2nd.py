ce6639a67a585bbad208532694ffd64456ece6a3
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-11 17:31:39 +0530

    Discription: To put link in program

---
+++ 2nd.py
@@ -0,0 +1,17 @@
+<!--Discription: To put link in program
+#input: Link
+#output: Link
+#Date: 11/06/21
+#Author : Om Deshmukh-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Link</title>
+</head>
+<body>
+	<h1 ><b>Om Deshmukh</b></h1>
+	<a href="https://www.linkedin.com/in/om-deshmukh-797792204/">linked In</a>
+</body>
+</html>
